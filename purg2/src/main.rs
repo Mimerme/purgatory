dd2225ec8b23c9d5b8523a988dc3eb1f4d0915bf
wannabe <wannabe@wannabe.wannabe.me>
2021-07-13 22:28:14 -0400

    Macroquad Client

---
+++ purg2/src/main.rs
@@ -0,0 +1,113 @@
+use macroquad::prelude::*;
+use std::{thread, time};
+
+
+enum Uniform {
+    Vec2(f32, f32),
+    Vec3(f32, f32, f32),
+    Vec4(f32, f32, f32, f32),
+    Float(f32),
+}
+
+#[macroquad::main("Shadertoy")]
+async fn main() {
+    let mut fragment_shader = DEFAULT_FRAGMENT_SHADER.to_string();
+    let mut vertex_shader = DEFAULT_VERTEX_SHADER.to_string();
+    let mut uniforms : Vec<(String, UniformType)> = vec![
+        ("iTime".to_string(), UniformType::Float1),
+        ("iTimeDelta".to_string(), UniformType::Float1),
+        ("iFrame".to_string(), UniformType::Int1),
+        ("iDate".to_string(), UniformType::Float4),
+        ("iMouse".to_string(), UniformType::Float4),
+        ("iResolution".to_string(), UniformType::Float2),
+    ];
+
+    let pipeline_params = PipelineParams {
+        depth_write: true,
+        depth_test: Comparison::LessOrEqual,
+        ..Default::default()
+    };
+
+    let mut material = load_material(
+        &vertex_shader,
+        &fragment_shader,
+        MaterialParams {
+            pipeline_params,
+            uniforms: uniforms.clone(),
+            ..Default::default()
+        },
+    )
+    .unwrap();
+
+    let mut camera = Camera3D {
+        position: vec3(-15., 15., -5.),
+        up: vec3(0., 1., 0.),
+        target: vec3(0., 5., -5.),
+        ..Default::default()
+    };
+
+    let mut time : f32 = 0.0;
+    let mut timeDelta : f32 = 0.0;
+    let mut mouse : [f32; 4] = [0.0, 0.0,0.0,0.0];
+    let mut date : [f32; 4] = [0.0, 0.0,0.0,0.0];
+    let mut resolution : [f32;2] = [0.0, 0.0];
+
+    loop {
+        clear_background(RED);
+        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
+        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
+        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
+
+        draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
+
+        set_camera(&camera);
+        gl_use_material(material);
+
+        // Update the uniforms on every frame here
+        material.set_uniform("iTime", time);
+        material.set_uniform("iTimeDelta", timeDelta);
+        material.set_uniform("iMouse",mouse);
+        material.set_uniform("iDate", date);
+        material.set_uniform("iResolution", resolution);
+
+        draw_plane(vec3(0., 2., 0.), vec2(5., 5.), Texture2D::empty(), WHITE);
+        set_default_camera();
+
+        thread::sleep(time::Duration::from_millis(15));
+        next_frame().await
+    }
+
+}
+
+const DEFAULT_FRAGMENT_SHADER: &'static str = "#version 100
+precision lowp float;
+
+varying vec2 uv;
+
+uniform sampler2D Texture;
+uniform float iTime;
+
+void main() {
+    gl_FragColor = texture2D(Texture, uv);
+    gl_FragColor = vec4(iTime, 0.0, 0.0, 1.0);
+}
+";
+
+const DEFAULT_VERTEX_SHADER: &'static str = "#version 100
+precision lowp float;
+
+attribute vec3 position;
+attribute vec2 texcoord;
+
+varying vec2 uv;
+
+uniform mat4 Model;
+uniform mat4 Projection;
+uniform float iTime;
+
+void main() {
+    gl_Position = Projection * Model * vec4(position, 1);
+    uv = texcoord;
+}
+";
+
